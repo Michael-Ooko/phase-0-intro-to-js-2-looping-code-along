242f2c5a1f73889b6941757b9a5d23bb56ae3044
Michael <oliengo.michael@gmail.com>
2024-04-07 14:55:58 +0300

    complete loop lap

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+// for loop
+function writeCards(namesArray, eventName) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < namesArray.length; i++) { 
+      let message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+
+  function countDown(num) {
+    while (num >= 0) { 
+      console.log(num);
+      num--; 
+    }
+  }
